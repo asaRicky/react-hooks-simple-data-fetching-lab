2e02d48befe59a7700aa09f4b2861bfdb3fe0ee3
asaRicky <derrickomondi208@gmail.com>
2023-05-06 08:38:23 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,33 @@
-// create your App component here
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
-    const [dogImage, setDogImage] = useState(null);
-    const [isLoading, setisLoading] = useState(true);
+  const [dogImage, setDogImage] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                setisLoading(true); // Set loading to true while fetching dod image
-                constbresponse = await fetch("https://dog.ceo/api/breeds/image/random");
-                const jsonData = await Response.json();
-                setDogImage(jsonData.message);
-                setisLoading(false);
-            } catch (error) {
-                console.error( "Error fetching data:", error);
-            }
-        };
-        fetchData();
-    }, []);
-
-    return (
-        <div>
-          {isLoading ? ( // Render "Loading..." when data is being fetched
-            <p>Loading...</p>
-          ) : (
-            // Render dog image when data is received
-            <img src={dogImage} alt="A Random Dog" />
-          )}
-        </div>
-      );
+  useEffect(() => {
+    const fetchDogImage = async () => {
+      try {
+        setIsLoading(true);
+        const response = await fetch("https://dog.ceo/api/breeds/image/random");
+        const data = await response.json();
+        setDogImage(data.message);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error fetching dog image:", error);
+      }
     };
-    
-    export default App
+    fetchDogImage();
+  }, []);
+
+  return (
+    <div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <img src={dogImage} alt="A Random Dog" />
+      )}
+    </div>
+  );
+};
+
+export default App;
